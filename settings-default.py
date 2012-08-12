d2142cd16b1d70c42a040def9bd0a9bb21f5063c
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2012-08-12 23:50:13 +0200

    Add files & modifications necessary for GAE
      -- modify urls to serve reviewit at '/'
      -- copy settings.py (relational) to settings-default.py
      -- make settings.py point to settings-gae.py
      -- etc.

---
+++ settings-default.py
@@ -0,0 +1,199 @@
+# Django settings for dj_intro_cs project.
+
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+install_dir = os.path.dirname(__file__)
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': install_dir + '/dbase.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Prague'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = install_dir+'/collected_static'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    install_dir+'/static',
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '*(6)g1q!scy-0bnlz9_2))0wg@_tz84um7v3ss)e*1@^ofuj*j'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE=True
+
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    
+    'common.http.HttpExceptionsMiddleware',
+)
+
+ROOT_URLCONF = 'dj_intro_cs.urls'
+
+TEMPLATE_DIRS = (
+    install_dir+'/templates',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'commentbin',
+    # Uncomment the next line to enable the admin:
+     'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+     'django.contrib.admindocs',
+     'south',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+import utils
+
+site_path=os.path.dirname(__file__)
+
+
+PER_HOST_SETTINGS = {
+  'development': { 
+      'DEBUG':True,
+      'TEMPLATE_DEBUG':True,
+      'install_dir':site_path,
+      'DATABASES': {
+	'default': {
+	  'ENGINE': 'django.db.backends.sqlite3',
+	  'NAME':os.path.join(site_path,'dbase.sqlite3'),
+	}
+       },
+      'TEMPLATE_DIRS' : (
+        os.path.join(site_path,'templates'),
+      ),
+      'INSTALLED_APPS': (
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'commentbin',
+	# Uncomment the next line to enable the admin:
+	'django.contrib.admin',
+	# Uncomment the next line to enable admin documentation:
+	'django.contrib.admindocs',
+	'south',
+      ),
+      'STATIC_URL':'/static/',
+      'STATICFILES_DIRS':(os.path.join(site_path,'static'),)
+   }
+}
+
+HOST='development'
+utils.load_settings(HOST,PER_HOST_SETTINGS,globals())
--- urls.py
+++ urls.py
@@ -9,12 +9,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dj_intro_cs.views.home', name='home'),
     # url(r'^dj_intro_cs/', include('dj_intro_cs.foo.urls')),
-    url(r'^commentbin/',include('commentbin.urls')),
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^commentbin/',include('commentbin.urls')),
+
+    ('^', include('commentbin.urls')),
 )
