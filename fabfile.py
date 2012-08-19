d5d3d4c46556083291797215d6b102bf6daea751
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2012-08-19 23:43:23 +0200

    Added a function to reload the pythonanywhere app.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,7 @@
-from fabric.api import local
+from fabric.api import *
+
+import urllib2
+import cookielib
 
 def push():
   local("git push origin")
@@ -8,3 +11,28 @@ def push():
 def deploy_static():
   local("./manage.py collectstatic --noinput")
   local("scp -r collected_static/* ffweb:~/public_html/commentbin_static/")
+  
+def reload_pythonanywhere():
+  try:
+    f = open('/home/jonathan/.kde/share/apps/kcookiejar/cookies','r')
+    sessionid=None
+    for l in f:
+      if 'pythonanywhere' in l and 'sessionid' in l:
+	sessionid = l.split()[-1]
+    if sessionid is None:
+      abort("Please log-in to pythonanywhere in Rekonq")
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+    req = urllib2.Request('https://www.pythonanywhere.com/reload_web_app')
+    req.add_header("Cookie","sessionid="+sessionid )
+    response = urllib2.urlopen(req)
+  except urllib2.URLError, e:
+    print "Network error:", e
+    return None
+  ret=response.read()
+  if ret == "OK":
+    return True
+  return False
+  
+  
