c9fa2ed6632aef92d8bb76fc7e9550bb6de20389
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2012-08-19 20:26:20 +0200

    Add a first version of pythonanywhere config.

--- settings-pythonanywhere.py
+++ settings-pythonanywhere.py
@@ -15,11 +15,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': install_dir + '/dbase.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'jonathanverner$reviewit',                      # Or path to database file if using sqlite3.
+        'USER': 'jonathanverner',                      # Not used with sqlite3.
+        'PASSWORD': 'pythonpass',                  # Not used with sqlite3.
+        'HOST': 'mysql.server',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
